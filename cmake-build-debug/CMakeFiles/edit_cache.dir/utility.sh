cd88593ce973dd33c117c04bd730fe59fd2b4ddd
syedaaryan <syedaaryans@gmail.com>
2024-11-03 15:57:45 +0530

    Ready for learning

--- cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/d/Projects/DSA/cmake-build-debug
-/cygdrive/c/Users/ASUS/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
--- cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/d/Projects/DSA/cmake-build-debug
-/cygdrive/c/Users/ASUS/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
